b2343af83f79f5f1d0919a49a6e3515052011d14
Peter Harrison <peter@palisadoes.org>
2020-05-11 15:26:54 -0700

    Added buttons

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,16 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int questionIndex = 0;
+
+  void answerQuestion(){
+    setState(() {
+      questionIndex += 1;
+    });
+    print(questionIndex);
+
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<String> questions = [
+      'What\'s your favourite colour?',
+      'What\'s your animal?',
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Quiz App'),
       ),
-      body: Column(
-        children: [
-          
-        ],
+      body: SafeArea(
+        child: Container(
+          child:Column(
+          children: [
+            Container(child: Text(questions[questionIndex]),),
+            Column(children: [
+              Icon(FontAwesomeIcons.question),
+              SizedBox(
+                height: 15.0,
+              ),
+              RaisedButton(onPressed: answerQuestion
+
+              )
+            ]),
+            ],
+),
+        ),
       ),
     );
   }
